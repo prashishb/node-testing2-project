115ab7a74f7c6f2f384003d5b5704230933c6eef
Prashish Bhandari <89494812+prashishb@users.noreply.github.com>
2021-12-16 06:48:38 -0500

    created basic users-model

---
+++ api/users/users-model.js
@@ -0,0 +1,29 @@
+const db = require('../../data/db-config');
+
+function getAll() {
+  return null;
+}
+
+function getById(id) {
+  return null;
+}
+
+async function insert(user) {
+  return null;
+}
+
+async function update(id, changes) {
+  return null;
+}
+
+function remove(id) {
+  return null;
+}
+
+module.exports = {
+  getAll,
+  getById,
+  insert,
+  update,
+  remove,
+};
